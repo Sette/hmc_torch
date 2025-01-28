78e5f9a4d0836061a4016239b843cf0d7dd9b4ba
Bruno Silva Sette <brunosilvasette@gmail.com>
2025-01-27 21:09:35 -0300

    :art: Reformat train_constraint.py to use multi gpu

--- hmc/model/train_constraint.py
+++ hmc/model/train_constraint.py
@@ -2,14 +2,15 @@
 from hmc.model import ConstrainedFFNNModel, get_constr_out
 from hmc.dataset import initialize_dataset
 import os
-##Imports for use more than 1 gpu and TPU
-import torch.distributed as dist
-import torch_xla.core.xla_model as xm
-
 
 import torch
 import torch.utils.data
+import torch.distributed as dist
 import torch.nn as nn
+
+import torch.multiprocessing as mp
+from torch.nn.parallel import DistributedDataParallel as DDP
+
 import random
 import argparse
 import numpy as np
@@ -19,66 +20,24 @@ from sklearn.impute import SimpleImputer
 from sklearn import preprocessing
 from sklearn.metrics import average_precision_score
 # from sklearn.metrics import f1_score, precision_recall_curve, roc_auc_score, auc
-os.environ['MASTER_ADDR'] = 'localhost'
-os.environ['MASTER_PORT'] = '12355'
-os.environ['WORLD_SIZE'] = '2'  # Número total de GPUs
-os.environ['RANK'] = str(0)  # Rank atual da GPU
 
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '5554'
 
-def train_constraint():
-    # Training settings
-    parser = argparse.ArgumentParser(description='Train neural network')
+    # initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
-    # Required  parameters
-    parser.add_argument('--dataset', type=str, required=True, 
-                        nargs='+', default=['seq_GO', 'derisi_GO', '0.6', '0.7'], 
-                        help='List with dataset names to train')
-    parser.add_argument('--dataset_path', type=str, required=True,
-                        help='dataset path')
-    parser.add_argument('--batch_size', type=int, required=True,
-                        help='input batch size for training')
-    parser.add_argument('--lr', type=float, required=True,
-                        help='learning rate')
-    parser.add_argument('--dropout', type=float, required=True,
-                        help='dropout probability')
-    parser.add_argument('--hidden_dim', type=int, required=True,
-                        help='size of the hidden layers')
-    parser.add_argument('--num_layers', type=int, required=True,
-                        help='number of hidden layers')
-    parser.add_argument('--weight_decay', type=float, required=True,
-                        help='weight decay')
-    parser.add_argument('--non_lin', type=str, required=True,
-                        help='non linearity function to be used in the hidden layers')
-    parser.add_argument('--output_path', type=str, required=True,
-                        help='output path')
-    parser.add_argument('--device', type=int, default=0,
-                        help='device (default:0)')
-    parser.add_argument('--num_epochs', type=int, default=2000,
-                        help='Max number of epochs to train (default:2000)')
-    parser.add_argument('--seed', type=int, default=0,
-                        help='random seed (default:0)')
-    
-    args = parser.parse_args()
-    hyperparams = {'batch_size': args.batch_size, 'num_layers': args.num_layers, 'dropout': args.dropout,
-                   'non_lin': args.non_lin, 'hidden_dim': args.hidden_dim, 'lr': args.lr,
-                   'weight_decay': args.weight_decay}
-    ## Insert her a logic to use all datasets with arguments
-    
-    print(type(args.dataset))
-    
-    if 'all' in args.dataset:
-        datasets = ['cellcycle_GO', 'derisi_GO', 'eisen_GO', 'expr_GO', 'gasch1_GO',
-                    'gasch2_GO', 'seq_GO', 'spo_GO', 'cellcycle_FUN', 'derisi_FUN', 
-                    'eisen_FUN', 'expr_FUN', 'gasch1_FUN', 'gasch2_FUN', 'seq_FUN', 'spo_FUN']
-    else:
-        if len(args.dataset) > 1:
-            datasets = [str(dataset) for dataset in args.dataset]
-        else:
-            datasets = [args.dataset]
-            assert ('_' in args.dataset)
-            assert ('FUN' in args.dataset or 'GO' in args.dataset or 'others' in args.dataset)
-        
+def cleanup():
+    dist.destroy_process_group()
 
+def run_train(train, world_size, datasets):
+    mp.spawn(train,
+             args=(world_size,datasets),
+             nprocs=world_size,
+             join=True)
+
+def train(rank, world_size, datasets):
     # Dictionaries with number of features and number of labels for each dataset
     input_dims = {'diatoms': 371, 'enron': 1001, 'imclef07a': 80, 'imclef07d': 80, 'cellcycle': 77, 'church': 27,
                   'derisi': 63, 'eisen': 79, 'expr': 560, 'gasch1': 173, 'gasch2': 52, 'hom': 47034, 'seq': 529,
@@ -89,30 +48,6 @@ def train_constraint():
                       'gasch2': 4128, 'hom': 4128, 'seq': 4130, 'spo': 4116}
     output_dims_others = {'diatoms': 398, 'enron': 56, 'imclef07a': 96, 'imclef07d': 46, 'reuters': 102}
     output_dims = {'FUN': output_dims_FUN, 'GO': output_dims_GO, 'others': output_dims_others}
-
-    # Set seed
-    seed = args.seed
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    random.seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    
-    # Verifica quantas GPUs estão disponíveis
-    num_gpus = torch.cuda.device_count()
-    print(f"Total de GPUs disponíveis: {num_gpus}")
-
-    # Verificar disponibilidade de TPU
-    tpu_device = xm.xla_device()
-    if tpu_device == "xla:0":
-        device = tpu_device
-        print(f"Dispositivo TPU: {device}")
-    elif num_gpus >= 1:
-        device = torch.device('cuda')
-    elif num_gpus == 0:
-        device = torch.device('cpu')
-        
 
     for dataset_name in datasets:
         print(".......................................")
@@ -215,8 +150,6 @@ def train_constraint():
                 # Clear gradients w.r.t. parameters
                 optimizer.zero_grad()
                 output = model(x.float())
-                if device == "xla:0":
-                    xm.mark_step()
 
                 constr_output = get_constr_out(output, R)
                 train_output = labels * output.double()
@@ -282,3 +215,65 @@ def train_constraint():
 
             if patience == 0:
                 break
+
+def run():
+    # Training settings
+    parser = argparse.ArgumentParser(description='Train neural network')
+
+    # Required  parameters
+    parser.add_argument('--dataset', type=str, required=True, 
+                        nargs='+', default=['seq_GO', 'derisi_GO', '0.6', '0.7'], 
+                        help='List with dataset names to train')
+    parser.add_argument('--dataset_path', type=str, required=True,
+                        help='dataset path')
+    parser.add_argument('--batch_size', type=int, required=True,
+                        help='input batch size for training')
+    parser.add_argument('--lr', type=float, required=True,
+                        help='learning rate')
+    parser.add_argument('--dropout', type=float, required=True,
+                        help='dropout probability')
+    parser.add_argument('--hidden_dim', type=int, required=True,
+                        help='size of the hidden layers')
+    parser.add_argument('--num_layers', type=int, required=True,
+                        help='number of hidden layers')
+    parser.add_argument('--weight_decay', type=float, required=True,
+                        help='weight decay')
+    parser.add_argument('--non_lin', type=str, required=True,
+                        help='non linearity function to be used in the hidden layers')
+    parser.add_argument('--output_path', type=str, required=True,
+                        help='output path')
+    parser.add_argument('--device', type=int, default=0,
+                        help='device (default:0)')
+    parser.add_argument('--num_epochs', type=int, default=2000,
+                        help='Max number of epochs to train (default:2000)')
+    parser.add_argument('--seed', type=int, default=0,
+                        help='random seed (default:0)')
+    
+    args = parser.parse_args()
+    hyperparams = {'batch_size': args.batch_size, 'num_layers': args.num_layers, 'dropout': args.dropout,
+                   'non_lin': args.non_lin, 'hidden_dim': args.hidden_dim, 'lr': args.lr,
+                   'weight_decay': args.weight_decay}
+    ## Insert her a logic to use all datasets with arguments
+    
+    print(type(args.dataset))
+    
+    if 'all' in args.dataset:
+        datasets = ['cellcycle_GO', 'derisi_GO', 'eisen_GO', 'expr_GO', 'gasch1_GO',
+                    'gasch2_GO', 'seq_GO', 'spo_GO', 'cellcycle_FUN', 'derisi_FUN', 
+                    'eisen_FUN', 'expr_FUN', 'gasch1_FUN', 'gasch2_FUN', 'seq_FUN', 'spo_FUN']
+    else:
+        if len(args.dataset) > 1:
+            datasets = [str(dataset) for dataset in args.dataset]
+        else:
+            datasets = [args.dataset]
+            assert ('_' in args.dataset)
+            assert ('FUN' in args.dataset or 'GO' in args.dataset or 'others' in args.dataset)
+
+    # Verifica quantas GPUs estão disponíveis
+    num_gpus = torch.cuda.device_count()
+    print(f"Total de GPUs disponíveis: {num_gpus}")
+
+    world_size = num_gpus
+
+    run_train(train, world_size, datasets)
+
