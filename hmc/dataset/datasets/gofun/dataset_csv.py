57e37bc04206c2df34091f2345f8de860cd8b961
Bruno Silva Sette <brunosilvasette@gmail.com>
2025-03-15 14:03:11 -0300

    :bug: Rollback to original preprocess go funcat

--- hmc/dataset/datasets/gofun/dataset_csv.py
+++ hmc/dataset/datasets/gofun/dataset_csv.py
@@ -21,33 +21,33 @@ def load_and_concat_csv_files(directory, sep='|'):
         file_path = os.path.join(directory, file)
         df = pd.read_csv(file_path, sep=sep)
         # Convert 'features' column from string representation of lists to actual lists
-        if 'features' in df.columns:
-            print('convertendo string em array')
-            df['features'] = df['features'].apply(lambda x: np.array(ast.literal_eval(x)) if isinstance(x, str) else x)
 
         dataframes.append(df)
 
     return pd.concat(dataframes, ignore_index=True) if dataframes else None
 
+
 class HMCDatasetCsv():
     def __init__(self, csv_path, is_go):
         self.df = pd.DataFrame()
-        self.X, self.Y = None, None
+        self.x, self.y = None, None
         self.is_go = is_go
         self.csv_path = csv_path
         self.parse_csv()
 
     def set_y(self, y):
-        self.Y = np.stack(y)
-        self.X = np.array(self.X)
+        self.y = np.stack(y)
+
+    def transform_features(self):
+        self.x = self.df.features.apply(lambda x : ast.literal_eval(x)).tolist()
 
     def parse_csv(self):
         #self.df = pd.read_csv(self.csv_path, sep='|')
         self.df = load_and_concat_csv_files(self.csv_path, sep='|')
         #X = df['features'].tolist()
         #self.df['features'] = self.df['features'].apply(json.loads)
-        self.X = self.df['features'].tolist()
-        self.Y = self.df['labels'].tolist()
+        self.y = self.df['labels'].tolist()
+        self.transform_features()
 
 
         #X = np.array([json.loads(x) for x in df['features']])
