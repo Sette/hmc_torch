1c50a75f1aa5302e06e3afe93cf79673d64032db
sette <brunosilvasette@gmail.com>
2025-06-11 19:18:34 -0300

    :book: Add docstring to save_dict_to_json for better clarity and usage examples

--- src/hmc/train/utils.py
+++ src/hmc/train/utils.py
@@ -109,5 +109,19 @@ def show_local_precision(local_precisions, set="Train"):
 
 
 def save_dict_to_json(dictionary, file_path):
+    """
+    Saves a dictionary to a JSON file.
+
+    Args:
+        dictionary (dict): The dictionary to be saved.
+        file_path (str): The path to the JSON file where the dictionary will be saved.
+
+    Raises:
+        TypeError: If the dictionary contains non-serializable objects.
+        OSError: If the file cannot be written.
+
+    Example:
+        save_dict_to_json({'a': 1, 'b': 2}, 'output.json')
+    """
     with open(file_path, "w", encoding="utf-8") as json_file:
         json.dump(dictionary, json_file, ensure_ascii=False, indent=4)
