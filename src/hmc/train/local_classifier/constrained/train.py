3288674ddb5f3e78b50ab29a58cdb4e8903c3091
sette <brunosilvasette@gmail.com>
2025-06-18 18:15:12 -0300

    refactor: Simplify optimizer setup and update gradient handling in train_step function

--- src/hmc/train/local_classifier/constrained/train.py
+++ src/hmc/train/local_classifier/constrained/train.py
@@ -63,15 +63,21 @@ def train_step(args):
     args.best_model = [None] * args.max_depth
     logging.info("Best val loss created %s", args.best_val_loss)
 
-    optimizers = [
-        torch.optim.Adam(
-            model.parameters(),
-            lr=args.lr_values[int(idx)],
-            weight_decay=args.weight_decay_values[int(idx)],
+    # optimizers = [
+    #     torch.optim.Adam(
+    #         model.parameters(),
+    #         lr=args.lr_values[int(idx)],
+    #         weight_decay=args.weight_decay_values[int(idx)],
+    #     )
+    #     for idx, model in args.model.levels.items()
+    # ]
+    # args.optimizers = optimizers
+
+    args.optimizers = torch.optim.Adam(
+            args.model.parameters(),
+            lr=args.lr_values[0],
+            weight_decay=args.weight_decay_values[0],
         )
-        for idx, model in args.model.levels.items()
-    ]
-    args.optimizers = optimizers
 
     for epoch in range(1, args.epochs + 1):
         args.model.train()
@@ -87,13 +93,13 @@ def train_step(args):
             outputs = args.model(inputs.float())
 
             # Zerar os gradientes antes de cada batch
-            # args.optimizer.zero_grad()
-            for optimizer in args.optimizers:
-                optimizer.zero_grad()
+            args.optimizers.zero_grad()
+            # for optimizer in args.optimizers:
+            #     optimizer.zero_grad()
             for index in args.active_levels:
                 if args.level_active[index]:
-                    output = outputs[str(index)]
-                    target = targets[index].float()
+                    output = outputs[str(index)].double()
+                    target = targets[index].double()
                     
                     # MCLoss
                     if index == 0:
@@ -104,16 +110,16 @@ def train_step(args):
                         train_output = target * output.double()
                         train_output = get_constr_out(train_output, R)
                         train_output = (1 - target) * constr_output.double() + target * train_output
-                        loss = args.criterions[index](train_output, target.double())
+                        loss = args.criterions[index](train_output, target)
                     local_train_losses[index] += loss
 
         # Backward pass (cálculo dos gradientes)
         for i, total_loss in enumerate(local_train_losses):
             if i in args.active_levels and args.level_active[i]:
                 total_loss.backward()
-        # args.optimizer.step()
-        for optimizer in args.optimizers:
-            optimizer.step()
+        args.optimizers.step()
+        #for optimizer in args.optimizers:
+        #    optimizer.step()
 
         local_train_losses = [
             loss / len(args.train_loader) for loss in local_train_losses
