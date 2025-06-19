6149db9297f80231e311eb34f72f14e95fb07bda
sette <brunosilvasette@gmail.com>
2025-06-19 17:39:59 -0300

    Enhance deployment script with git clone and dependency installation options for improved user experience

--- deploy_kaggle.sh
+++ deploy_kaggle.sh
@@ -20,6 +20,26 @@ if [ "$PYTORCH_MODE" != "cuda" ] && [ "$PYTORCH_MODE" != "cpu" ]; then
     exit 1
 fi
 
+
+# Pergunta ao usuário se deseja executar git pull antes de rodar o treinamento
+echo -n "Deseja clonar o repositório antes de iniciar o treinamento? (s/n): "
+read GIT_CLONE_CHOICE
+GIT_CLONE_CHOICE=$(echo "$GIT_CLONE_CHOICE" | tr '[:upper:]' '[:lower:]')  # Converte para minúsculas
+
+if [ "$GIT_CLONE_CHOICE" = "s" ]; then
+    echo "Executando git clone no servidor..."
+    ssh "$REMOTE_HOST" "
+        export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+        git clone -b develop --single-branch https://github.com/Sette/hmc-torch.git $REMOTE_PATH &&
+        cd $REMOTE_PATH &&
+        git config core.sshCommand 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' \
+    "
+else
+    echo "Pulando git clone."
+fi
+
+
+
 # Pergunta ao usuário se deseja executar git pull antes de rodar o treinamento
 echo -n "Deseja executar git pull antes de iniciar o treinamento? (s/n): "
 read GIT_PULL_CHOICE
@@ -30,11 +50,73 @@ if [ "$GIT_PULL_CHOICE" = "s" ]; then
     ssh "$REMOTE_HOST" "
         export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
         cd $REMOTE_PATH &&
-        git pull
+        git pull \
     "
 else
     echo "Pulando git pull."
 fi
+
+
+# Ask the user if they want to create a local virtual environment
+echo -n "Do you want to create a local venv? (y/n): "
+read INSTALL_VENV_CHOICE
+INSTALL_VENV_CHOICE=$(echo "$INSTALL_VENV_CHOICE" | tr '[:upper:]' '[:lower:]')
+# Check if the user wants to install Poetry using pip
+if [ "$INSTALL_VENV_CHOICE" = "y" ]; then
+    ssh "$REMOTE_HOST" "
+        source ~/.bashrc &&
+        apt install -y python3.10-venv &&
+        cd $REMOTE_PATH &&
+        python3 -m venv .venv \
+    "
+else
+    echo "Skipping venv installation."
+fi
+
+
+
+# Ask the user if they want to install packages with poetry
+echo -n "Do you want to Poetry with pip? (y/n): "
+read INSTALL_POETRY_CHOICE
+INSTALL_POETRY_CHOICE=$(echo "$INSTALL_POETRY_CHOICE" | tr '[:upper:]' '[:lower:]')
+
+# Check if the user wants to install Poetry using pip
+if [ "$INSTALL_POETRY_CHOICE" = "y" ]; then
+    echo "Installing Poetry with pip..."
+    python3 -m pip install poetry
+    ssh "$REMOTE_HOST" "
+        source ~/.bashrc &&
+        cd $REMOTE_PATH &&
+        source .venv/bin/activate &&
+        pip install pip --upgrade &&
+        pip install --upgrade pip setuptools wheel &&
+        pip install poetry \
+        
+    "
+else
+    echo "Skipping Poetry installation."
+fi
+
+
+echo -n "Do you want to install dependencies with Poetry? (y/n): "
+read INSTALL_CHOICE
+INSTALL_CHOICE=$(echo "$INSTALL_CHOICE" | tr '[:upper:]' '[:lower:]')
+
+if [ "$INSTALL_CHOICE" = "y" ]; then
+    echo "Instalando dependências com Poetry..."
+    ssh "$REMOTE_HOST" "
+        source ~/.bashrc &&
+        cd $REMOTE_PATH &&
+        source .venv/bin/activate &&
+        poetry source add pytorch --priority=explicit &&
+        poetry source remove pytorch-cpu || true &&
+        poetry install \
+    "
+else
+    echo "Pulando instalação de dependências."
+fi
+
+
 
 # Pergunta ao usuário se deseja executar com tmux ou sem tmux
 echo -n "Deseja executar com tmux? (s/n): "
@@ -48,11 +130,7 @@ if [ "$TMUX_CHOICE" = "s" ]; then
         tmux has-session -t $TMUX_SESSION 2>/dev/null || tmux new-session -d -s $TMUX_SESSION
         tmux send-keys -t $TMUX_SESSION 'source ~/.bashrc &&
         cd $REMOTE_PATH &&
-        python3 -m venv .venv &&
         source .venv/bin/activate &&
-        poetry source add pytorch --priority=explicit &&
-        poetry source remove pytorch-cpu || true &&
-        poetry install --no-root &&
         chmod +x $SCRIPT_TO_RUN && ./train.sh --device cuda' C-m
     "
     echo "Treinamento iniciado dentro do tmux! Sessão: '$TMUX_SESSION'."
@@ -60,21 +138,9 @@ else
     echo "Executando o script de treinamento diretamente..."
     echo "Usando PyTorch com CUDA..."
     ssh "$REMOTE_HOST" "
-        source ~/.bashrc &&
-        apt install -y python3.10-venv &&
-        apt install -y python3.10-pip &&
-        pip install pip --upgrade &&
-        pip install pipx &&
-        pipx ensurepath &&
-        pipx install poetry &&
-        source ~/.bashrc &&
         cd $REMOTE_PATH &&
-        python3 -m venv .venv &&
         source .venv/bin/activate &&
-        poetry source add pytorch --priority=explicit &&
-        poetry source remove pytorch-cpu || true &&
-        poetry install --no-root &&
-        chmod +x $SCRIPT_TO_RUN && ./train.sh --device cuda
+        chmod +x $SCRIPT_TO_RUN && ./$SCRIPT_TO_RUN --device cuda --dataset_path /kaggle/input/gene-ontology-original \
     "
     echo "Treinamento terminado sem tmux."
 fi
